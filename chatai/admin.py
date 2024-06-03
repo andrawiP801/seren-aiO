10971f7f059afdbc1de039222da0f554c2834704
AndrawiP801 <perez.marquez.david.andrawi@gmail.com>
2024-06-03 16:48:01 -0600

    ´prueba foro

--- chatai/admin.py
+++ chatai/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, SiteConfig, Chat
+from .models import User, SiteConfig, Chat, Book, ChatMessage
 
 # Register your models here.
 admin.site.register(User)
 
 admin.site.register(SiteConfig)
 admin.site.register(Chat)
+admin.site.register(Book)
+admin.site.register(ChatMessage)
