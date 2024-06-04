277ccb117cdcda075bee39deab5b55e60992857f
andrawiP801 <perez.marquez.david.andrawi@gmail.com>
2024-06-04 09:33:13 -0500

    cambios biblioteca

--- SerenAI/urls.py
+++ SerenAI/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chatai.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=False)
