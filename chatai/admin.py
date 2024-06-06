b8bf18d2f1b1b289c8fc9e06e7557b798bd0f232
AndrawiP801 <perez.marquez.david.andrawi@gmail.com>
2024-06-05 19:01:11 -0600

    servicio page

--- chatai/admin.py
+++ chatai/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, SiteConfig, Chat, Book, ChatMessage, EmotionLog
+from .models import User, SiteConfig, Chat, Book, ChatMessage, EmotionLog, Message, FAQ
 
 # Register your models here.
 admin.site.register(User)
@@ -12,3 +12,5 @@ admin.site.register(Chat)
 admin.site.register(Book, BookAdmin)
 admin.site.register(ChatMessage)
 admin.site.register(EmotionLog)
+admin.site.register(Message)
+admin.site.register(FAQ)
